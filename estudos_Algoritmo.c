e353309f76095eeb1148b1622c5a60fc709ecde2
Barretovisk <matheusbsoliveira18@gmail.com>
2026-04-14 18:48:09 -0300

    Add files via upload
    2. Desenvolva uma função que receba um número inteiro e retorne seu dobro.

---
+++ estudos_Algoritmo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+ int dobro(int n1){
+      return n1 * 2;
+ }
+
+int main(){
+  
+    int a, b = 0;
+    
+    scanf("%d", &a);
+    
+    b = dobro(a);
+    
+    printf("%d", b);
+    
+    return 0;
+}
